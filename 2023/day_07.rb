1490acab7468e4dc5023812422789cc004a0acc6
Dalton Dick <daltondick2010@gmail.com>
2023-12-07 11:44:38 -0600

    Day 7 Part 2

--- 2023/day_07.rb
+++ 2023/day_07.rb
@@ -26,7 +26,7 @@ class PokerHand
   HAND_TYPES = %i[
     FIVE_OF_A_KIND
     FOUR_OF_A_KIND
-    FULL_OF_HOUSE
+    FULL_HOUSE
     THREE_OF_A_KIND
     TWO_PAIR
     ONE_PAIR
@@ -58,7 +58,7 @@ class PokerHand
                        if self.cards.count(uniq.first) == 4
                          :FOUR_OF_A_KIND
                        else
-                         :FULL_OF_HOUSE
+                         :FULL_HOUSE
                        end
                      elsif uniq_count == 3
                        if self.cards.count(uniq.first) == 3
@@ -106,7 +106,103 @@ class PokerHand
   end
 end
 
-class DumbPokerHand < PokerHand
+class JokerPokerHand
+  attr_accessor :cards,
+                :hand_type,
+                :card_order,
+                :bid,
+                :hand_type_rank
+
+  CARD_RANKS = [
+    ['A', 14],
+    ['K', 13],
+    ['Q', 12],
+    ['T', 11],
+    ['9', 10],
+    ['8', 9],
+    ['7', 8],
+    ['6', 7],
+    ['5', 6],
+    ['4', 5],
+    ['3', 4],
+    ['2', 3],
+    ['J', 2]
+  ].to_h.freeze
+
+  HAND_TYPES = %i[
+    FIVE_OF_A_KIND
+    FOUR_OF_A_KIND
+    FULL_HOUSE
+    THREE_OF_A_KIND
+    TWO_PAIR
+    ONE_PAIR
+    HIGH_CARD
+  ].freeze
+
+  HAND_TYPE_ORDER = HAND_TYPES.zip((1..HAND_TYPES.length).to_a.reverse).to_h.freeze
+
+  def initialize(cards, bid)
+    self.cards = cards.split('')
+    self.bid = bid
+
+    uniq = self.cards.uniq.reject { |c| c == 'J' }
+    uniq = uniq.sort do |a, b|
+      result = self.cards.count(b) <=> self.cards.count(a)
+    end
+
+    count = uniq.map { |c| self.cards.count(c) }
+
+    self.card_order = self.cards.map { |c| rank_of_card(c) }
+
+    self.hand_type = if count[0] == 5
+                       :FIVE_OF_A_KIND
+                     elsif count[0] == 4
+                       :FOUR_OF_A_KIND
+                     elsif count[0] == 3
+                       if count[1] == 2
+                         :FULL_HOUSE
+                       else
+                         :THREE_OF_A_KIND
+                       end
+                     elsif count[0] == 2
+                       if count[1] == 2
+                         :TWO_PAIR
+                       else
+                         :ONE_PAIR
+                       end
+                     elsif count[0] == 1
+                       :HIGH_CARD
+                     else
+                       :HIGH_CARD
+                     end
+
+    num_jokers = self.cards.count('J')
+    num_jokers.times.each do
+      case hand_type
+      when :FOUR_OF_A_KIND
+        self.hand_type = :FIVE_OF_A_KIND
+      when :THREE_OF_A_KIND
+        self.hand_type = :FOUR_OF_A_KIND
+      when :TWO_PAIR
+        self.hand_type = :FULL_HOUSE
+      when :ONE_PAIR
+        self.hand_type = :THREE_OF_A_KIND
+      when :HIGH_CARD
+        self.hand_type = :ONE_PAIR
+      end
+    end
+
+    self.hand_type_rank = rank_of_hand(hand_type)
+  end
+
+  def inspect
+    "#{cards.inspect} #{bid.inspect} #{hand_type} #{hand_type_rank} #{card_order.inspect}}"
+  end
+
+  def to_s
+    inspect
+  end
+
   def <=>(other)
     result = other.hand_type_rank <=> hand_type_rank
     if result == 0
@@ -117,6 +213,16 @@ class DumbPokerHand < PokerHand
     end
 
     result
+  end
+
+  private
+
+  def rank_of_card(card)
+    CARD_RANKS[card]
+  end
+
+  def rank_of_hand(hand_type)
+    HAND_TYPE_ORDER[hand_type]
   end
 end
 
@@ -133,18 +239,22 @@ class Day07 < Base
   end
 
   def part1(input)
-    poker_hands = parse_input(input, DumbPokerHand)
+    poker_hands = parse_input(input, PokerHand)
     sorted = poker_hands.sort
-    # sorted.each do |s|
-    #   puts s
-    # end
     zipped = (1..sorted.length).to_a.reverse.zip(sorted)
     zipped.map { |z| z[0] * z[1].bid }.sum
   end
 
-  def part2(input); end
+  def part2(input)
+    poker_hands = parse_input(input, JokerPokerHand)
+    sorted = poker_hands.sort
+    zipped = (1..sorted.length).to_a.reverse.zip(sorted)
+    zipped.map { |z| z[0] * z[1].bid }.sum
+  end
 end
 
 day = Day07.new
 puts "Example: #{day.part1(day.example_input_a)}"
 puts "Part 1: #{day.part1(day.input)}"
+puts "Example: #{day.part2(day.example_input_a)}"
+puts "Part 2: #{day.part2(day.input)}"
