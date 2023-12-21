772ecda6ca36226825c245831a28511718a9950c
Dalton Dick <daltondick2010@gmail.com>
2023-12-21 08:27:20 -0600

    Day 15 Part 2

--- 2023/day_15.rb
+++ 2023/day_15.rb
@@ -30,10 +30,48 @@ class Day15 < Base
   def part_2(input)
     data = parse_input(input)
 
-    0
+    boxes = []
+    256.times do |i|
+      boxes[i] = []
+    end
+
+    data.each do |lens|
+      split = lens.split(/=|-/)
+      lens_name = split[0]
+      focal_point = split[1]&.to_i
+
+      cur = 0
+      lens_name.chars.each do |c|
+        ord = c.ord
+        cur = ((cur + ord) * 17) % 256
+      end
+
+      if lens.include?('=')
+        index = boxes[cur].find_index { |x| x[0] == lens_name }
+        if index
+          boxes[cur][index][1] = focal_point
+        else
+          boxes[cur].push([lens_name, focal_point])
+        end
+      else
+        boxes[cur].delete_if { |b| b[0] == lens_name }
+      end
+    end
+
+    total = 0
+    boxes.each_with_index do |box, i|
+      box.each_with_index do |lens, j|
+        lens_total = (i + 1) * (j + 1) * lens[1]
+        total += lens_total
+      end
+    end
+
+    total
   end
 end
 
 day = Day15.new
 puts "Example: #{day.part_1(day.example_input_a)}"
 puts "Example: #{day.part_1(day.input)}"
+puts "Example: #{day.part_2(day.example_input_a)}"
+puts "Part 2: #{day.part_2(day.input)}"
