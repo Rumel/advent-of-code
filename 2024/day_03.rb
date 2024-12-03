c3a54439483a43db8df939bb8b1fbcf75dd6f95f
Dalton Dick <daltondick2010@gmail.com>
2024-12-03 12:31:01 -0600

    Day 3 Part 2

--- 2024/day_03.rb
+++ 2024/day_03.rb
@@ -32,8 +32,33 @@ class Day03 < Base # rubocop:disable Style/Documentation
     total
   end
 
-  def part_2(input) # rubocop:disable Naming/VariableNumber
-    parsed = parse_input(input)
+  def part_2(file) # rubocop:disable Naming/VariableNumber
+    parsed = parse_input(file)
+
+    total = 0
+
+    is_on = true
+    s = parsed
+    loop do
+      matches = /(mul\((\d{1,3}),(\d{1,3})\))|(do\(\))|(don't\(\))/.match(s)
+      break unless matches
+
+      if matches[0] == 'do()'
+        is_on = true
+      elsif matches[0] == "don't()"
+        is_on = false
+      elsif is_on
+        total += matches[2].to_i * matches[3].to_i
+      end
+
+      index = s.index(matches[0]) + matches[0].length
+
+      break if index >= s.length
+
+      s = s[index..]
+    end
+
+    total
   end
 end
 
@@ -41,5 +66,5 @@ day = Day03.new
 
 puts "Example 1: #{day.part_1(day.example_input_a)}"
 puts "Part 1: #{day.part_1(day.input)}"
-# puts "Example 2: #{day.part_2(day.example_input_a)}"
-# puts "Part 2: #{day.part_2(day.input)}"
+puts "Example 2: #{day.part_2(day.example_input_b)}"
+puts "Part 2: #{day.part_2(day.input)}"
