87f56de0a3958f4c336892a0a7f97d31fc2353a1
Dalton Dick <daltondick2010@gmail.com>
2024-12-05 17:12:55 -0600

    Temp code

--- 2024/day_05.rb
+++ 2024/day_05.rb
@@ -56,12 +56,35 @@ class Day05 < Base # rubocop:disable Style/Documentation
 
   def part_2(file) # rubocop:disable Naming/VariableNumber
     graph = parse_input(file)
+    second_graph = graph.dup
+    second_graph.delete 'updates'
+    order = second_graph.map { |k, v| [k, v.length] }.sort_by { |k, v| -v }.map(&:first)
+    # order << second_graph.select { |k, v| v.length == 1 }.to_a[0][1][0]
+
+    count = 0
+    graph['updates'].each do |update|
+      next unless get_value(graph, update) == 0
+
+      new_order = []
+      order.each do |o|
+        new_order << o if update.include? o
+      end
+      l = new_order.length
+
+      count += if l.even?
+                 new_order[(l + 1) / 2]
+               else
+                 new_order[l / 2]
+               end
+    end
+
+    count
   end
 end
 
 day = Day05.new
 
-puts "Example 1: #{day.part_1(day.example_input_a)}"
-puts "Part 1: #{day.part_1(day.input)}"
-# puts "Example 2: #{day.part_2(day.example_input_a)}"
-# puts "Part 2: #{day.part_2(day.input)}"
+# puts "Example 1: #{day.part_1(day.example_input_a)}"
+# puts "Part 1: #{day.part_1(day.input)}"
+puts "Example 2: #{day.part_2(day.example_input_a)}"
+puts "Part 2: #{day.part_2(day.input)}"
